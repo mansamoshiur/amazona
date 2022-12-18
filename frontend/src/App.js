64423be17d009ef712a5710f3d48a4836fa8ee8f
mansamoshiur <mansamoshiur@gmail.com>
2022-12-19 05:16:55 +0600

    amazona-v1-Creat-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href='/' >amazona</a>
+      </header>
+      <main>
+        product list
+      </main>
+    </div>
+  );
+}
+
+export default App;
